c405dbf55ed39741c6f1a6ace0b1ab084d0ae859
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-11-06 10:50:05 +0900

    Delete unnecessary json code

--- app/controllers/anger_records_controller.rb
+++ app/controllers/anger_records_controller.rb
@@ -2,13 +2,11 @@ class AngerRecordsController < ApplicationController
   before_action :set_anger_record, only: [:show, :edit, :update, :destroy]
 
   # GET /anger_records
-  # GET /anger_records.json
   def index
     @anger_records = AngerRecord.all
   end
 
   # GET /anger_records/1
-  # GET /anger_records/1.json
   def show
   end
 
@@ -22,42 +20,34 @@ class AngerRecordsController < ApplicationController
   end
 
   # POST /anger_records
-  # POST /anger_records.json
   def create
     @anger_record = AngerRecord.new(anger_record_params)
 
     respond_to do |format|
       if @anger_record.save
         format.html { redirect_to @anger_record, notice: 'Anger record was successfully created.' }
-        format.json { render :show, status: :created, location: @anger_record }
       else
         format.html { render :new }
-        format.json { render json: @anger_record.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /anger_records/1
-  # PATCH/PUT /anger_records/1.json
   def update
     respond_to do |format|
       if @anger_record.update(anger_record_params)
         format.html { redirect_to @anger_record, notice: 'Anger record was successfully updated.' }
-        format.json { render :show, status: :ok, location: @anger_record }
       else
         format.html { render :edit }
-        format.json { render json: @anger_record.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /anger_records/1
-  # DELETE /anger_records/1.json
   def destroy
     @anger_record.destroy
     respond_to do |format|
       format.html { redirect_to anger_records_url, notice: 'Anger record was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
