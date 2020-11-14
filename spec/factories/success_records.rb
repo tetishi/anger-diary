b6fe2dea18bdaa9091a5555a68bafa32dc8fc910
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-11-14 17:11:33 +0900

    Add feature spec for success records

--- spec/factories/success_records.rb
+++ spec/factories/success_records.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+require "faker"
+require "forgery_ja"
+
 FactoryBot.define do
   factory :success_record do
-    body { "MyText" }
+    body { Faker::Lorem.sentence }
     succeed_at { "2020-11-11" }
   end
 end
