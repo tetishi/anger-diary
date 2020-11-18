0a450478484bb1f5613967941e1e0a61ef01e4f8
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-11-18 23:22:42 +0900

    Add links to each day of a calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,9 +2,15 @@
 
 class CalendarsController < ApplicationController
   def index
-    @calendars = AngerRecord.all
+    # binding.pry
+    # @calendars = AngerRecord.all
+    @anger_records = AngerRecord.all
+    @success_records = SuccessRecord.all
   end
 
   def show
+    # @record_date = AngerRecord.find(params[:id])
+    # @record_date = AngerRecord.where(params[:got_angry_on])
+    # redirect_to calendar_path(id: :got_angry_on)
   end
 end
