6bc29da47c5ffdeab9a4d5798ab831c79edf9291
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-11-20 14:52:40 +0900

    Change path to calendar/:date

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@
 
 class CalendarsController < ApplicationController
   def index
-    # binding.pry
+    binding.pry
     # @calendars = AngerRecord.all
     @anger_records = AngerRecord.all
     @success_records = SuccessRecord.all
@@ -10,6 +10,10 @@ class CalendarsController < ApplicationController
 
   def show
     # @record_date = AngerRecord.find(params[:id])
+    # @record_date = AngerRecord.joins(:got_angry_on).where( })
+    @record_date = AngerRecord.where(got_angry_on: Date.today)
+    # @record_date = AngerRecord.new
+    # @record_date = AngerRecord.where(:got_angry_on)
     # @record_date = AngerRecord.where(params[:got_angry_on])
     # redirect_to calendar_path(id: :got_angry_on)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   resources :anger_records
   resources :success_records
-  resources :calendars, only: [:index, :show]
+  resources :calendars, only: [:index, :show], param: :date
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
