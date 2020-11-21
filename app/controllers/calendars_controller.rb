0531359ec4661fc9a5ea5bbf89d15dec36834fc0
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-11-21 15:44:38 +0900

    Fix ActionView::Template::Error

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,18 +3,15 @@
 class CalendarsController < ApplicationController
   def index
     # binding.pry
-    # @calendars = AngerRecord.all
-    @record_dates = AngerRecord.all
-    @record_dates = SuccessRecord.all
+    # @anger_records = AngerRecord.all
+    # @success_records = SuccessRecord.all
+    @record_dates = AngerRecord.all && SuccessRecord.all
   end
 
   def show
-    # @record_date = AngerRecord.find(params[:id])
-    # @record_date = AngerRecord.joins(:got_angry_on).where( })
+    # @got_angry_on = AngerRecord.find(params[:got_angry_on])
+    # @record_date = AngerRecord.where(got_angry_on: @got_angry_on)
     @record_date = AngerRecord.where(got_angry_on: Date.today)
-    # @record_date = AngerRecord.new
-    # @record_date = AngerRecord.where(:got_angry_on)
-    # @record_date = AngerRecord.where(params[:got_angry_on])
     # redirect_to calendar_path(id: :got_angry_on)
   end
 end
