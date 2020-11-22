084efa74b6db7afbb84b6a3c4cd8711a88710593
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-11-22 12:09:41 +0900

    Fix calendar_path and modify calendars_controller

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -5,12 +5,46 @@ class CalendarsController < ApplicationController
     # binding.pry
     # @anger_records = AngerRecord.all
     # @success_records = SuccessRecord.all
-    @record_dates = AngerRecord.all && SuccessRecord.all
+    # @record_dates = AngerRecord.all && SuccessRecord.all
+    @anger_records = AngerRecord.where(got_angry_on: AngerRecord.all.pluck(:got_angry_on).map(&:to_date))
+    @anger_dates = @anger_records.pluck(:got_angry_on).map {|date| date.to_s}
+    @anger_date = @anger_dates.last
+
+    # @success_records = SuccessRecord.where(created_at: SuccessRecord.all.pluck(:created_at).map(&:to_date))
+    @success_dates = SuccessRecord.all.pluck(:created_at).map(&:to_date)
+    @success_dates = @success_dates.uniq.map {|date| date.to_s}
+    @success_date = @success_dates.last
+
+    all_dates = (@anger_dates | @success_dates).uniq.map {|date| date.to_s}
+    @success_records = SuccessRecord.where("created_at between ? and ?", all_dates.first.to_datetime.beginning_of_day, all_dates.last.to_datetime.end_of_day)
   end
 
   def show
-    @anger_date = AngerRecord.includes(:level, :got_angry_on).where(got_angry_on: Date.today)
-    @success_date = SuccessRecord.where(created_at: Time.current.all_day)
-    # redirect_to calendar_path(id: :got_angry_on)
+    # binding.pry
+    # all_dates = (@success_records).uniq.map {|date| date.to_s}
+    @anger_records = AngerRecord.where(got_angry_on: AngerRecord.all.pluck(:got_angry_on))
+   
+    # @anger_dates = @anger_date.uniq.map {|date| date.to_s}
+    
+    @anger_dates = @anger_records.pluck(:got_angry_on).map {|date| date.to_s}
+
+    
+
+    @success_dates = SuccessRecord.all.pluck(:created_at).map(&:to_date)
+    @success_dates = @success_dates.uniq.map {|date| date.to_s}
+
+    all_dates = (@anger_dates | @success_dates).uniq.map {|date| date.to_s}
+    @success_records = SuccessRecord.where("created_at between ? and ?", all_dates.first.to_datetime.beginning_of_day, all_dates.last.to_datetime.end_of_day)
+    # @anger_record = AngerRecord.find(params[:date])
+    
+    # @anger_level = @anger_dates.pluck()
+    # @success_dates = SuccessRecord.where(created_at: Time.current.all_day)
+    
+    # @success_dates = SuccessRecord.where(created_at: SuccessRecord.all.pluck(:created_at).map(&:to_date))
+    # @success_date = SuccessRecord.all.pluck(:created_at).map(&:to_date)
+
+    # @success_date = @success_date.uniq.map{|date| date.to_s}
+    # # all_dates = (@anger_date | @success_date).uniq.map{|date| date.to_s}
+    # @success_date = SuccessRecord.where("created_at between ? and ?", all_dates.first.to_datetime.beginning_of_day, all_dates.last.to_datetime.end_of_day)
   end
 end
