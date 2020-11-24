80bb07ed619663ed43cf2a9115bdc17254472401
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-11-24 23:02:40 +0900

    Delete unnecessary code

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,24 +2,14 @@
 
 class CalendarsController < ApplicationController
   def index
-    @anger_records = AngerRecord.all
-    @anger_dates = AngerRecord.all.pluck(:got_angry_on).map {|date| date.to_s}
+    @anger_dates = AngerRecord.all.pluck(:got_angry_on).map { |date| date.to_s }
+    @success_dates = SuccessRecord.all.pluck(:created_at).map(&:to_date)
+    @success_dates = @success_dates.uniq.map { |date| date.to_s }
 
-    @success_records = SuccessRecord.all
-    @success_dates = @success_records.pluck(:created_at).map(&:to_date)
-    @success_dates = @success_dates.uniq.map {|date| date.to_s}
-
-    @record_dates = (@anger_dates | @success_dates).map {|date| date.to_s}.uniq
+    @record_dates = (@anger_dates | @success_dates).map { |date| date.to_s }.uniq
   end
 
   def show
-    @anger_dates = AngerRecord.all.pluck(:got_angry_on).map {|date| date.to_s}
-
-    @success_dates = SuccessRecord.all.pluck(:created_at).map(&:to_date)
-    @success_dates = @success_dates.uniq.map {|date| date.to_s}
-
-    @record_dates = (@anger_dates | @success_dates).map {|date| date.to_s}.uniq
-
     @anger_record = AngerRecord.find_by(got_angry_on: params[:date])
     @success_record = SuccessRecord.find_by(succeeded_on: params[:date])
   end
