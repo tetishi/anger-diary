aed2231ef62bf3a6664e9fa96c897859b7efe4f3
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-11-27 21:43:06 +0900

    Generate home controller and put root to the index of it

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root "home#index"
   resources :anger_records
   resources :success_records
   resources :calendars, only: [:index, :show], param: :date
