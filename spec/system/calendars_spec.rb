7bce065e5faf4f9bcba67a9564a1a3c22e6da936
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-11-27 12:06:07 +0900

    Add records_support and refactor calendars_spec

--- spec/system/calendars_spec.rb
+++ spec/system/calendars_spec.rb
@@ -15,32 +15,23 @@ feature "Calendars", js: true, type: :feature do
 
   context "with only an anger record" do
     scenario "seeing an anger record on a calendar" do
-      visit anger_records_path
-      click_on "怒りを記録する"
-      select @anger_record.level, from: "怒りのレベル"
-      fill_in "怒った日時", with: @anger_record.got_angry_on
-      within "#anger_hour" do
-        select @anger_record.got_angry_at.strftime("%H")
-      end
-      fill_in "場所", with: @anger_record.place
-      fill_in "内容", with: @anger_record.body
-      find("input[name='anger_record[changeable]'][value='Yes']").set(@anger_record.changeable)
-      find("input[name='anger_record[important]'][value='Yes']").set(@anger_record.important)
-      click_on "登録する"
-
-      assert_text "怒りの記録が作成されました。"
-      click_on "戻る"
+      create_anger_record
 
       visit calendar_path(@anger_record.got_angry_on)
 
       expect(page).to have_text @anger_record.got_angry_on.to_s
-      expect(page).to have_selector "p", text: @anger_record.level
-      expect(page).to have_selector "p", text: @anger_record.got_angry_on
-      expect(page).to have_selector "p", text: @anger_record.got_angry_at.strftime("%k")
-      expect(page).to have_selector "p", text: @anger_record.place
-      expect(page).to have_selector "p", text: @anger_record.body
-      expect(page).to have_selector "p", text: @anger_record.changeable
-      expect(page).to have_selector "p", text: @anger_record.important
+      selectors = [
+        @anger_record.level,
+        @anger_record.got_angry_on,
+        @anger_record.got_angry_at.strftime("%k"),
+        @anger_record.place,
+        @anger_record.body,
+        @anger_record.changeable,
+        @anger_record.important
+      ]
+      selectors.each do |selector|
+        expect(page).to have_selector "p", text: selector
+      end
       expect(page).to_not have_selector "p", text: "出来たこと"
 
       visit anger_records_path
@@ -54,23 +45,22 @@ feature "Calendars", js: true, type: :feature do
 
   context "with only a success record" do
     scenario "seeing a success record on a calendar" do
-      visit success_records_path
-      click_on "今日出来たことを記録する"
-      fill_in "今日出来たこと", with: @success_record.body
-      click_on "登録する"
-
-      assert_text "今日出来たことが作成されました。"
-      click_on "戻る"
+      create_success_record
 
       visit calendar_path(Date.today.to_s)
 
       expect(page).to have_selector "body", text: Date.today.to_s
-      expect(page).to_not have_selector "p", text: "怒りのレベル"
-      expect(page).to_not have_selector "p", text: "怒った日時"
-      expect(page).to_not have_selector "p", text: "場所"
-      expect(page).to_not have_selector "p", text: "内容"
-      expect(page).to_not have_selector "p", text: "変えられる内容か"
-      expect(page).to_not have_selector "p", text: "重要な内容か"
+      selectors = [
+        "怒りのレベル",
+        "怒った日時",
+        "場所",
+        "内容",
+        "変えられる内容か",
+        "重要な内容か"
+      ]
+      selectors.each do |selector|
+        expect(page).to_not have_selector "p", text: selector
+      end
       expect(page).to have_selector "p", text: @success_record.body
 
       visit success_records_path
@@ -100,24 +90,23 @@ feature "Calendars", js: true, type: :feature do
       assert_text "怒りの記録が作成されました。"
       click_on "戻る"
 
-      visit success_records_path
-      click_on "今日出来たことを記録する"
-      fill_in "今日出来たこと", with: @success_record.body
-      click_on "登録する"
-
-      assert_text "今日出来たことが作成されました。"
-      click_on "戻る"
+      create_success_record
 
       visit calendar_path(Date.today.to_s)
       expect(page).to have_text Date.today.to_s
-      expect(page).to have_selector "p", text: @anger_record.level
-      expect(page).to have_selector "p", text: Date.today.to_s
-      expect(page).to have_selector "p", text: @anger_record.got_angry_at.strftime("%k")
-      expect(page).to have_selector "p", text: @anger_record.place
-      expect(page).to have_selector "p", text: @anger_record.body
-      expect(page).to have_selector "p", text: @anger_record.changeable
-      expect(page).to have_selector "p", text: @anger_record.important
-      expect(page).to have_selector "p", text: @success_record.body
+      selectors = [
+        @anger_record.level,
+        Date.today.to_s,
+        @anger_record.got_angry_at.strftime("%k"),
+        @anger_record.place,
+        @anger_record.body,
+        @anger_record.changeable,
+        @anger_record.important,
+        @success_record.body
+      ]
+      selectors.each do |selector|
+        expect(page).to have_selector "p", text: selector
+      end
     end
   end
 end
