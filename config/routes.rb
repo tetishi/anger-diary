7f0788d8b3c5426664a90cdbfd76b9a7fa89be5e
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-04 14:29:00 +0900

    Add patch for edit_calendar_path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   root "home#index"
   resources :anger_records, except: :index
   resources :success_records, except: :index
-  resources :calendars, except: [:new, :create], param: :date
+  resources :calendars, except: [:new, :create], param: :date do
+    member do
+      get "edit"
+      patch "edit"
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
