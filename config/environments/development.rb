3f20aa04ddc279cdcf7dcbe6f8810854c99232db
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-09 20:40:06 +0900

    Add confirmable for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,13 +33,22 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   user_name: 'tetsuroishida1112@gmail.com',
+  #   password: 'F3n54w5n',
+  #   authentication: :plain,
+  #   enable_starttls_auto: true 
+  # }
+
+  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
