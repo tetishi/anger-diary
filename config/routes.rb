7380cd5d8c2bf56334b109d80285993b1653c919
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-09 11:23:58 +0900

    Add devise for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root "home#index"
   resources :anger_records, except: [:index, :delete]
   resources :success_records, except: [:index, :delete]
