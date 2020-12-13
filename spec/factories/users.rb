9662dfd85396a78e4e71e5f3e15263fb1e61efe3
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-13 18:17:30 +0900

    Add data in factories/users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
+require "faker"
+require "forgery_ja"
+
 FactoryBot.define do
   factory :user do
+    username { Faker::Name.name }
+    email { Faker::Internet.email }
+    encrypted_password { Faker::Internet.password(min_length: 6) }
   end
 end
