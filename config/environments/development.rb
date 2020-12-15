7c481b98072e4b2382ed6c6310857455ac251e7b
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-15 21:33:23 +0900

    Make password and username to env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    user_name: "tetsuroishida1112@gmail.com",
-    password: "rccphmckoikxdqza",
+    user_name: ENV["GMAIL_USER_NAME"],
+    password: ENV["GMAIL_PASSWORD"],
     authentication: :plain,
     enable_starttls_auto: true
   }
