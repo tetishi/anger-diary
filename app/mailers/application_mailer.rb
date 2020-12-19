225a0557cb6ae25adf551fdab0d417c126a9d7ab
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-19 16:38:57 +0900

    Modify email feature

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "support@anger-diary.herokuapp.com"
   layout "mailer"
 end
