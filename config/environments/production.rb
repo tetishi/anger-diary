fa3c746b736057689b97f66b8c13aaf87464b1fd
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-20 17:41:00 +0900

    Change gmail to sendgrid for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,12 +72,12 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    user_name: ENV["GMAIL_USER_NAME"],
-    password: ENV["GMAIL_PASSWORD"],
-    domain: "smtp.gmail.com",
-    authentication: :login,
+    address: "smtp.sendgrid.com",
+    port: 25,
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: "heroku.com",
+    authentication: :plain,
     enable_starttls_auto: true
   }
 
