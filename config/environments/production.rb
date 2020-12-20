6550b857205ec78c1d386541b1aac693ce76da36
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-20 11:10:46 +0900

    Fix gmail password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     user_name: ENV["GMAIL_USER_NAME"],
-    password: ENV["SENDGRID_PASSWORD"],
+    password: ENV["GMAIL_PASSWORD"],
     domain: "gmail.com",
     authentication: :plain,
     enable_starttls_auto: true
