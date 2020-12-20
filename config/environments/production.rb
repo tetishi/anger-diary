935fa7059a6dbc24f07a723d272f832f097e053f
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-20 11:07:04 +0900

    Modify username and password for gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    user_name: ENV["GMAIL_PASSWORD"],
-    password: ENV["GMAIL_USER_NAME"],
+    user_name: ENV["GMAIL_USER_NAME"],
+    password: ENV["SENDGRID_PASSWORD"],
     domain: "gmail.com",
     authentication: :plain,
     enable_starttls_auto: true
