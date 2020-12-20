39338479cf6cb43c9109c9ad56c5f6290c601d5b
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-20 18:00:04 +0900

    Modify mail setting for production

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,15 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.sendgrid.com",
+    port: 3000,
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: "heroku.com",
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+end
