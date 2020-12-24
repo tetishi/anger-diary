f7f26318f3d732ead799cd53178ebee12bb7b152
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-24 22:16:15 +0900

    Delete edit and update of calendars controller

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -17,51 +17,6 @@ class CalendarsController < ApplicationController
     @success_record = current_user.success_records.find_by(succeeded_on: params[:date])
   end
 
-  def edit
-    @anger_record = current_user.anger_records.find_by(got_angry_on: params[:date])
-    # @anger_records = current_user.anger_records.where(got_angry_on: params[:date]).sort_by do |anger_record|
-    #   anger_record.got_angry_at.strftime("%k")
-    # end
-    @success_record = current_user.success_records.find_by(succeeded_on: params[:date])
-    # binding.pry
-    # @record_date = @anger_record.try(:got_angry_on) || @success_record.try(:succeeded_on)
-  end
-
-  def update
-    @anger_record = current_user.anger_records.find_by(got_angry_on: params[:date])
-    @success_record = current_user.success_records.find_by(succeeded_on: params[:date])
-  end
-
-  # def destroy
-  #   @anger_records = current_user.anger_records.where(got_angry_on: params[:date]).sort_by do |anger_record|
-  #     anger_record.got_angry_at.strftime("%k")
-  #   end
-  #   @anger_record = current_user.anger_records.find_by(got_angry_on: params[:date])
-  #   @success_record = current_user.success_records.find_by(succeeded_on: params[:date])
-
-    # if @anger_records.present? && @success_record.present?
-    #   # binding.pry
-    #   if @anger_record.destroy
-    #     redirect_to calendars_url, notice: "怒りの記録が削除されました。"
-    #   elsif @success_record.destroy
-    #     redirect_to calendars_url, notice: "今日出来たことの記録が削除されました。"
-    #   end
-    # elsif @anger_records.present?
-    #   @anger_record.destroy
-    #   redirect_to calendars_url, notice: "怒りの記録が削除されました。"
-    # elsif @success_record.present?
-    #   @success_record.destroy
-    #   redirect_to calendars_url, notice: "今日出来たことの記録が削除されました。"
-    # end
-
-  #   # if @anger_record.destroy
-  #   #   redirect_to calendars_url, notice: "怒りの記録が削除されました。"
-  #   # end
-  #   # if @success_record.destroy
-  #   #   redirect_to calendars_url, notice: "今日出来たことの記録が削除されました。"
-  #   # end
-  # end
-
     private
 
       def anger_record_params
