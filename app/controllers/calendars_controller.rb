a90bb5fa277091006b1f7b7e610053d45575e3f2
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-30 21:52:30 +0900

    Use fetch api for displaying records

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -7,6 +7,14 @@ class CalendarsController < ApplicationController
     @anger_dates = current_user.anger_records.pluck(:got_angry_on).map(&:to_s)
     @success_dates = current_user.success_records.pluck(:succeeded_on).map(&:to_s)
     @record_dates = (@anger_dates | @success_dates).uniq
+
+    # @anger_records = current_user.anger_records.where(got_angry_on: params[:date]).sort_by do |anger_record|
+    #   anger_record.got_angry_at.strftime("%k")
+    # end
+    # @anger_record = current_user.anger_records.find_by(got_angry_on: params[:date])
+    # @success_record = current_user.success_records.find_by(succeeded_on: params[:date])
+
+    # render json: @anger_records, @success_record
   end
 
   def show
