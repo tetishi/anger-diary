744b45526d5701329769752433e83b1669f1a15a
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2020-12-31 22:01:34 +0900

    Add if statements in then

--- app/javascript/calendars.js
+++ app/javascript/calendars.js
@@ -1,13 +1,18 @@
-fetch("http://localhost:300/calendars", {
+fetch("http://localhost:300/calendars.json", {
   method: "GET"
 })
   .then(response => {
     return response.json();
 })
   .then(json => {
-    json.forEach(c => {
-      
-    });
+    if(this.anger_record) {
+      json.forEach(c => {
+        this.anger_record.push(c);
+      });
+    }
+    if(this.success_record) {
+      return this.success_record
+    }
   })
   .catch(error => {
     console.error(error)
