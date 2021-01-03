b1d26e31915c2de461ff4510594ba88acbf53ee6
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2021-01-03 21:20:22 +0900

    Modify url for fetch

--- app/javascript/calendars.js
+++ app/javascript/calendars.js
@@ -6,22 +6,26 @@
 // {e.preventDefault();console.log("hi")});
 
 document.addEventListener('DOMContentLoaded', function(){
-  const links = document.querySelectorAll(".records");
+  const links = document.querySelector(".records");
+  const calendarURL = "http://localhost:3000/calendars.json"
+  let recordDate = this.anger_record.got_angry_on
 
   // anger_record.got_angry_onのリソースをとってくる
   console.log(links);
   links.addEventListener("click", function(e) {
     e.preventDefault();
     console.log("hi");
-    fetch("http://localhost:3000/calendars.json?hoge=20201030")
-    // fetch(`http://localhost:3000/calendars.json?record=${anger_record.got_angry_on}`)
+    // fetch("http://localhost:3000/calendars.json?hoge=20201030")
+    // fetch(`http://localhost:3000/calendars.json?record=${record_date}`)
+    fetch(calendarURL + "/" + recordDate)
     // それぞれの日付のanger_record.got_angry_onを渡す
       .then(response => response.json())
       .then(anger_records => {
         console.log("test");
-        console.log(anger_records)
+        console.log(anger_records);
         for (const anger_record of anger_records) {
           return anger_record
+          // データをとってきてArrayにいれる
         }
       })
       .catch(error => alert(error));
