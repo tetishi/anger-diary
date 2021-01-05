ee12f7634892eb0f309c6d74638e7546e8435922
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2021-01-05 19:48:17 +0900

    Modify then for displaying records

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,19 +8,19 @@ class CalendarsController < ApplicationController
     @success_dates = current_user.success_records.pluck(:succeeded_on).map(&:to_s)
     @record_dates = (@anger_dates | @success_dates).uniq
 
-    # @anger_records = current_user.anger_records.where(got_angry_on: params[:date]).sort_by do |anger_record|
-    #   anger_record.got_angry_at.strftime("%k")
-    # end
+    @anger_records = current_user.anger_records.where(got_angry_on: params[:date]).sort_by do |anger_record|
+      anger_record.got_angry_at.strftime("%k")
+    end
 
-    # @anger_record = current_user.anger_records.find_by(got_angry_on: params[:date])
-    # @success_record = current_user.success_records.find_by(succeeded_on: params[:date])
+    @anger_record = current_user.anger_records.find_by(got_angry_on: params[:date])
+    @success_record = current_user.success_records.find_by(succeeded_on: params[:date])
 
-    @anger_records = current_user.anger_records
+    @records = current_user.anger_records
     # binding.pry
     respond_to do |format|
       format.html
       # format.json {render :json => {hello: "World"}.to_json}
-      format.json {render :json => @anger_records.to_json}
+      format.json {render :json => @records.to_json}
     end
   end
 
@@ -35,13 +35,13 @@ class CalendarsController < ApplicationController
     @success_record = current_user.success_records.find_by(succeeded_on: params[:date])
   end
 
-    private
+    # private
 
-      def anger_record_params
-        params.require(:anger_or_success_data).permit(:level, :got_angry_on, :got_angry_at, :place, :anger_body, :changeable, :important)
-      end
+      # def anger_record_params
+      #   params.require(:anger_or_success_data).permit(:level, :got_angry_on, :got_angry_at, :place, :anger_body, :changeable, :important)
+      # end
 
-      def success_record_params
-        params.require(:anger_or_success_data).permit(:success_body)
-      end
+      # def success_record_params
+      #   params.require(:anger_or_success_data).permit(:success_body)
+      # end
 end
