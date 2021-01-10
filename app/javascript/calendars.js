473e1a9dcc6113ae8cc8734b3f157815f50aaeb8
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2021-01-10 23:42:59 +0900

    Use only one innerHTML at a time

--- app/javascript/calendars.js
+++ app/javascript/calendars.js
@@ -25,98 +25,158 @@ document.addEventListener('DOMContentLoaded', function(){
         // anger_records = JSON.parse(anger_records);
 
         var addRecord = function(anger_record) {
-          // console.log(anger_record[0]);
-          var level = document.createElement("p");
-          level.className = "media"
-          var record1 = document.createElement("li");
-          record1.className = "media-left"
+          var level = document.createElement("div");
+          // level.className = "media"
+          // var record1 = document.createElement("li");
+          // record1.className = "media-left"
 
-          var got_angry_on = document.createElement("p");
-          got_angry_on.className = "media"
-          var record2 = document.createElement("li");
-          record2.className = "media-left"
+          var got_angry_on = document.createElement("div");
+          // got_angry_on.className = "media"
+          // var record2 = document.createElement("li");
+          // record2.className = "media-left"
 
-          var got_angry_at = document.createElement("p");
-          var record3 = document.createElement("p");
-          record3.className = "media-left"
+          // var got_angry_at = document.createElement("div");
+          // var record3 = document.createElement("p");
+          // record3.className = "media-left"
 
-          var place = document.createElement("p");
-          place.className = "media"
-          var record4 = document.createElement("li");
-          record4.className = "media-left"
+          var place = document.createElement("div");
+          // place.className = "media"
+          // var record4 = document.createElement("li");
+          // record4.className = "media-left"
   
-          var anger_body = document.createElement("p");
-          anger_body.className = "media"
-          var record5 = document.createElement("li");
-          record5.className = "media-left"
+          var anger_body = document.createElement("div");
+          // anger_body.className = "media"
+          // var record5 = document.createElement("li");
+          // record5.className = "media-left"
 
-          var changeable = document.createElement("p");
-          changeable.className = "media"
-          var record6 = document.createElement("li");
-          record6.className = "media-left"
+          var changeable = document.createElement("div");
+          // changeable.className = "media"
+          // var record6 = document.createElement("li");
+          // record6.className = "media-left"
 
-          var important = document.createElement("p");
-          important.className = "media"
-          var record7 = document.createElement("li");
-          record7.className = "media-left"
+          var important = document.createElement("div");
+          // important.className = "media"
+          // var record7 = document.createElement("li");
+          // record7.className = "media-left"
 
           var buttons = document.createElement("div");
-          buttons.className = "field is-grouped my-5"
-          var editButton = document.createElement("div");
-          editButton.className = "control"
-          var deleteButton = document.createElement("div");
-          deleteButton.className = "control"
+          // buttons.className = "field is-grouped my-5"
+          // var editButton = document.createElement("div");
+          // editButton.className = "control"
+          // var deleteButton = document.createElement("div");
+          // deleteButton.className = "control"
 
-          editButton.innerHTML = ""
          
-          level.innerHTML = "<strong class='media-left'>怒りのレベル:</strong>"
-          record1.innerText = anger_record["level"]
+          level.innerHTML = "<p class='media'>" +
+                            "<strong class='media-left'>" +
+                            "怒りのレベル:" +
+                            "</strong><spam class='media-left'>" +
+                            "</spam>" +
+                            anger_record["level"] +
+                            "</p>"
+          // record1.innerText = anger_record["level"]
+
           // record1.innerText = anger_record.sort()
+          
 
-          got_angry_on.innerHTML = "<strong class='media-left'>怒った日時:</strong>"
-          record2.innerText = anger_record["got_angry_on"]
+          got_angry_on.innerHTML = "<p class='media'>" +
+                                   "<strong class='media-left'>" +
+                                   "怒った日時:" +
+                                   "</strong>" +
+                                   "<spam class='media-left'>" +
+                                   "</spam>" +
+                                   anger_record["got_angry_on"] +
+                                   " " +
+                                   new Date(anger_record["got_angry_at"]).getHours(); +
+                                   "時ごろ" + 
+                                   "</p>"
+          // record2.innerText = anger_record["got_angry_on"]
 
-          got_angry_at.innerHTML = "時ごろ"
+          // got_angry_at.innerHTML = "時ごろ"
           // record3.innerHTML = "<time datetime='hh'></time>"
           // record3.innerHTML = "<time datetime='hh'>anger_record['got_angry_at']</time>"
-          record3.innerHTML = anger_record["got_angry_at"]
-          record3 = new Date(anger_record["got_angry_at"]);
-          record3 = record3.getHours();
+          // record3.innerHTML = anger_record["got_angry_at"]
+
+          // record3.innerHTML = new Date(anger_record["got_angry_at"]).getHours();
+
           // debugger;
 
-          place.innerHTML = "<strong class='media-left'>場所:</strong>"
-          record4.innerText = anger_record["place"]
+          place.innerHTML = "<p class='media'>" +
+                            "<strong class='media-left'>" +
+                            "場所:" +
+                            "</strong>" +
+                            "<spam class='media-left'>" +
+                            "</spam>" +
+                            anger_record["place"] +
+                            "</p>"
+          // record4.innerText = anger_record["place"]
 
-          anger_body.innerHTML = "<strong class='media-left'>内容:</strong>"
-          record5.innerText = anger_record["anger_body"]
+          anger_body.innerHTML = "<p class='media'>" +
+                                 "<strong class='media-left'>" +
+                                 "内容:" +
+                                 "</strong>" +
+                                 "<spam class='media-left'>" +
+                                 "</spam>" +
+                                 anger_record["anger_body"] +
+                                 "</p>"
+          // record5.innerText = anger_record["anger_body"]
 
-          changeable.innerHTML = "<strong class='media-left'>変えらる内容か:</strong>"
-          record6.innerText = anger_record["changeable"]
+          changeable.innerHTML = "<p class='media'>" +
+                                 "<strong class='media-left'>" +
+                                 "変えらる内容か:" +
+                                 "</strong>" +
+                                 "<spam class='media-left'>" +
+                                 "</spam>" +
+                                 anger_record["changeable"] +
+                                 "</p>"
+          // record6.innerText = anger_record["changeable"]
 
-          important.innerHTML = "<strong class='media-left'>重要な内容か:</strong>"
-          record7.innerText = anger_record["important"]
-          // console.log(record.innerText);
+          important.innerHTML = "<p class='media'>" +
+                                "<strong class='media-left'>" +
+                                "重要な内容か:" +
+                                "</strong>" +
+                                "<spam class='media-left'>" +
+                                "</spam>" +
+                                anger_record["important"] +
+                                "</p>"
+          // record7.innerText = anger_record["important"]
+
+          buttons.innerHTML = "<div class='field is-grouped my-5'>" +
+                                 "<div class='control'>" +
+                                 "<a class='button is-primary is-size-5' href='/calendars/2021-01-07/anger_records/5/edit'>" +
+                                 "編集" +
+                                 "</a>" +
+                                 "</div>" +
+                                 "<div class='control'>" +
+                                 "<a data-confirm='本当によろしいですか？' class='button is-danger is-size-5' rel='nofollow' data-method='delete' href='/anger_records/5'>" +
+                                 "削除" +
+                                 "</a>" +
+                                 "</div>" +
+                                 "</div>"
+
           document.querySelector(".records").appendChild(level);
-          level.appendChild(record1);
+          // level.appendChild(record1);
 
           // debugger;
           document.querySelector(".records").appendChild(got_angry_on);
-          got_angry_on.appendChild(record2);
-          got_angry_on.appendChild(record3);
-          document.querySelector(".records").appendChild(got_angry_at);
+          // got_angry_on.appendChild(record2);
+          // got_angry_on.appendChild(record3);
+          // document.querySelector(".records").appendChild(got_angry_at);
           // document.querySelector(".records").appendChild(moment(record3).format('HH'));
 
           document.querySelector(".records").appendChild(place);
-          place.appendChild(record4);
+          // place.appendChild(record4);
 
           document.querySelector(".records").appendChild(anger_body);
-          anger_body.appendChild(record5);
+          // anger_body.appendChild(record5);
 
           document.querySelector(".records").appendChild(changeable);
-          changeable.appendChild(record6);
+          // changeable.appendChild(record6);
 
           document.querySelector(".records").appendChild(important);
-          important.appendChild(record7);
+          // important.appendChild(record7);
+
+          document.querySelector(".record-buttons").appendChild(buttons);
         }
         anger_records.forEach(function(anger_record){addRecord(anger_record)})
       })
