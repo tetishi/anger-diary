c1f86bde245e0b1574c6b597e6c4304828de8af5
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2021-01-11 22:53:55 +0900

    Modify code for fetch

--- app/javascript/calendars.js
+++ app/javascript/calendars.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 
   // var links = document.querySelectorAll(".dates");
-  var calendarURL = "http://localhost:3000/calendars.json"
+  // var calendarURL = "http://localhost:3000/calendars.json"
 
   document.querySelectorAll(".dates").forEach(function (links) {
     links.addEventListener("click", function(e) {
@@ -10,25 +10,20 @@ document.addEventListener('DOMContentLoaded', function(){
     // fetch("http://localhost:3000/calendars.json?hoge=20201030")
     // fetch(`http://localhost:3000/calendars.json?record=${record_date}`)
     // fetch(calendarURL + "/" + recordDate)
-      fetch(calendarURL)
+      fetch("http://localhost:3000/calendars.json")
         .then(response => response.json())
         .then(anger_records => {
           console.log("test");
           console.log(anger_records); // anger_recordsがjsonデータ
+          // debugger;
 
           var addRecord = function(anger_record) {
             var level = document.createElement("div");
-
             var got_angry_on = document.createElement("div");
-
             var place = document.createElement("div");
-  
             var anger_body = document.createElement("div");
-
             var changeable = document.createElement("div");
-
             var important = document.createElement("div");
-
             var buttons = document.createElement("div");
          
             level.innerHTML = "<p class='media'>" +
@@ -96,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function(){
                                  "</a>" +
                                  "</div>" +
                                  "<div class='control'>" +
-                                 "<a data-confirm='本当によろしいですか？' class='button is-danger is-size-5' rel='nofollow' data-method='delete' href='/anger_records/5'>" +
+                                 "<a data-confirm='本当によろしいですか？' class='button is-danger is-size-5' rel='nofollow' data-method='delete' href='/anger_records/:id'>" +
                                  "削除" +
                                  "</a>" +
                                  "</div>" +
@@ -104,19 +99,14 @@ document.addEventListener('DOMContentLoaded', function(){
                                  //  edit_calendar_anger_record_path(anger_record.got_angry_on, anger_record.id)
 
             document.querySelector(".records").appendChild(level);
-
             document.querySelector(".records").appendChild(got_angry_on);
-
             document.querySelector(".records").appendChild(place);
-
             document.querySelector(".records").appendChild(anger_body);
-
             document.querySelector(".records").appendChild(changeable);
-
             document.querySelector(".records").appendChild(important);
-
             document.querySelector(".records").appendChild(buttons);
           }
+          // debugger;
           anger_records.forEach(function(anger_record){addRecord(anger_record)})
         })
         .catch(error => alert(error));
