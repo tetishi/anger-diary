e92f5d682fd04c27aff4a874e93789981bb3db81
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2021-02-06 13:28:14 +0900

    Get id of success record

--- app/javascript/calendars.js
+++ app/javascript/calendars.js
@@ -1,21 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.querySelectorAll(".edit-button").forEach(function (editButton) {
     editButton.addEventListener("click", function() {
-    //   var 
-      debugger;
-      fetch(`http://localhost3000/anger_records/${this.id}/edit`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify()
-      })
-      .then(response => {
-        return response.json()
-      })
-      .then(data =>
-        console.log(data)
-      );
+      // debugger;
+      // const successRecordId = this.dataset('success-record-id');
+      const successRecordId = document.getElementById('success-record').getAttribute('data-record');
+      // debugger;
+      console.log(successRecordId);
+
+      // debugger;
+      // fetch(`http://localhost3000/anger_records/${this.id}/edit`, {
+      //   method: "PATCH",
+      //   headers: {
+      //     "Content-Type": "application/json"
+      //   },
+      //   body: JSON.stringify()
+      // })
+      // .then(response => {
+      //   return response.json()
+      // })
+      // .then(data =>
+      //   console.log(data)
+      // );
     })
   })
 })
