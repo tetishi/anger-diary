3d49ae2f1797c64f0543973613d19dba24bedf97
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2021-02-21 18:28:24 +0900

    Apply eslint

--- app/javascript/success_records.js
+++ app/javascript/success_records.js
@@ -1,117 +1,117 @@
-document.addEventListener("turbolinks:load", function() {
-  const successEditButton = document.querySelector(".js-success-edit-button");
-  if(successEditButton) {
-    successEditButton.addEventListener("click", function() {
-      const successLabelArea = document.getElementById('js-success-label');
-      const twoSuccessButton = document.getElementById('js-two-success-button');
-      const successTextArea = document.getElementById('js-success-textarea');
-      const successUpdateCancelButton = document.getElementById('js-success-update-cancel-button');
-  
-      successLabelArea.style.display = 'none';
-      twoSuccessButton.style.display = 'none';
-      successTextArea.style.display = '';
-      successUpdateCancelButton.style.display = '';
+document.addEventListener('turbolinks:load', function () {
+  const successEditButton = document.querySelector('.js-success-edit-button')
+  if (successEditButton) {
+    successEditButton.addEventListener('click', function () {
+      const successLabelArea = document.getElementById('js-success-label')
+      const twoSuccessButton = document.getElementById('js-two-success-button')
+      const successTextArea = document.getElementById('js-success-textarea')
+      const successUpdateCancelButton = document.getElementById('js-success-update-cancel-button')
+
+      successLabelArea.style.display = 'none'
+      twoSuccessButton.style.display = 'none'
+      successTextArea.style.display = ''
+      successUpdateCancelButton.style.display = ''
     })
   }
-  
-  const successCancelButton = document.getElementById("success-cancel-button");
-  if(successCancelButton) {
-    successCancelButton.addEventListener("click", function() {
-      const successLabelArea = document.getElementById('js-success-label');
-      const twoSuccessButton = document.getElementById('js-two-success-button');
-      const successTextArea = document.getElementById('js-success-textarea');
-      const successUpdateCancelButton = document.getElementById('js-success-update-cancel-button');
-      const successRecordError = document.getElementById('js-success-record-post-error');
-  
-      successLabelArea.style.display = '';
-      twoSuccessButton.style.display = '';
-      successTextArea.style.display = 'none';
-      successUpdateCancelButton.style.display = 'none';
-      successRecordError.style.display = 'none';
+
+  const successCancelButton = document.getElementById('success-cancel-button')
+  if (successCancelButton) {
+    successCancelButton.addEventListener('click', function () {
+      const successLabelArea = document.getElementById('js-success-label')
+      const twoSuccessButton = document.getElementById('js-two-success-button')
+      const successTextArea = document.getElementById('js-success-textarea')
+      const successUpdateCancelButton = document.getElementById('js-success-update-cancel-button')
+      const successRecordError = document.getElementById('js-success-record-post-error')
+
+      successLabelArea.style.display = ''
+      twoSuccessButton.style.display = ''
+      successTextArea.style.display = 'none'
+      successUpdateCancelButton.style.display = 'none'
+      successRecordError.style.display = 'none'
     })
   }
-  
-  const idSubmitUpdateButton = document.querySelector(".id-submit-update-button");
-  if(idSubmitUpdateButton) {
-    idSubmitUpdateButton.addEventListener("click", function() {
-      const successRecordId = document.getElementById('id-success-record').getAttribute('data-record');
-      const textField = document.getElementById('js-textarea-body-' + successRecordId);
+
+  const idSubmitUpdateButton = document.querySelector('.id-submit-update-button')
+  if (idSubmitUpdateButton) {
+    idSubmitUpdateButton.addEventListener('click', function () {
+      const successRecordId = document.getElementById('id-success-record').getAttribute('data-record')
+      const textField = document.getElementById(`js-textarea-body-${successRecordId}`)
       const body = textField.value
-  
-      let bodyData = {
-        'success_body': body
+
+      const bodyData = {
+        success_body: body
       }
-  
+
       fetch(`/success_records/${successRecordId}`, {
-        method: "PATCH",
+        method: 'PATCH',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify(bodyData)
       })
-      .then(response => response.json())
-      .then(data => {
-        const successLabelArea = document.getElementById('js-success-label');
-        const twoSuccessButton = document.getElementById('js-two-success-button');
-        const successTextArea = document.getElementById('js-success-textarea');
-        const successUpdateCancelButton = document.getElementById('js-success-update-cancel-button');
-        const successRecordError = document.getElementById('js-success-record-post-error');
-  
-        successLabelArea.style.display = '';
-        successLabelArea.textContent = data.success_body;
-        twoSuccessButton.style.display = '';
-        successTextArea.style.display = 'none';
-        successUpdateCancelButton.style.display = 'none';
-        successRecordError.style.display = 'none';
-      })
-      .catch(error => {
-        console.log(error.message)
-        const successRecordError = document.getElementById('js-success-record-post-error');
-        successRecordError.style.display = '';
-      })
+        .then((response) => response.json())
+        .then((data) => {
+          const successLabelArea = document.getElementById('js-success-label')
+          const twoSuccessButton = document.getElementById('js-two-success-button')
+          const successTextArea = document.getElementById('js-success-textarea')
+          const successUpdateCancelButton = document.getElementById('js-success-update-cancel-button')
+          const successRecordError = document.getElementById('js-success-record-post-error')
+
+          successLabelArea.style.display = ''
+          successLabelArea.textContent = data.success_body
+          twoSuccessButton.style.display = ''
+          successTextArea.style.display = 'none'
+          successUpdateCancelButton.style.display = 'none'
+          successRecordError.style.display = 'none'
+        })
+        .catch((error) => {
+          console.log(error.message)
+          const successRecordError = document.getElementById('js-success-record-post-error')
+          successRecordError.style.display = ''
+        })
     })
   }
-  
-  const submitUpdateButton = document.querySelector(".submit-update-button");
-  if(submitUpdateButton) {
-    submitUpdateButton.addEventListener("click", function() {
-      const successRecordDate = document.getElementById('success-record').getAttribute('data-record');
-      const textField = document.getElementById('js-textarea-body-' + successRecordDate);
+
+  const submitUpdateButton = document.querySelector('.submit-update-button')
+  if (submitUpdateButton) {
+    submitUpdateButton.addEventListener('click', function () {
+      const successRecordDate = document.getElementById('success-record').getAttribute('data-record')
+      const textField = document.getElementById(`js-textarea-body-${successRecordDate}`)
       const body = textField.value
-  
-      let bodyData = {
-        'success_body': body
+
+      const bodyData = {
+        success_body: body
       }
 
       fetch(`/calendars/${successRecordDate}`, {
-        method: "PATCH",
+        method: 'PATCH',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify(bodyData)
       })
-      .then(response => response.json())
-      .then(data => {
-        const successLabelArea = document.getElementById('js-success-label');
-        const twoSuccessButton = document.getElementById('js-two-success-button');
-        const successTextArea = document.getElementById('js-success-textarea');
-        const successUpdateCancelButton = document.getElementById('js-success-update-cancel-button');
-        const successRecordError = document.getElementById('js-success-record-post-error');
-        const successNotice = document.querySelector('.notice');
-  
-        successLabelArea.style.display = '';
-        successLabelArea.textContent = data.success_body;
-        twoSuccessButton.style.display = '';
-        successTextArea.style.display = 'none';
-        successUpdateCancelButton.style.display = 'none';
-        successRecordError.style.display = 'none';
-        successNotice.innerHTML = "<div>出来たことが編集されました。</div>"
-      })
-      .catch(error => {
-        console.log(error.message)
-        const successRecordError = document.getElementById('js-success-record-post-error');
-        successRecordError.style.display = '';
-      })
+        .then((response) => response.json())
+        .then((data) => {
+          const successLabelArea = document.getElementById('js-success-label')
+          const twoSuccessButton = document.getElementById('js-two-success-button')
+          const successTextArea = document.getElementById('js-success-textarea')
+          const successUpdateCancelButton = document.getElementById('js-success-update-cancel-button')
+          const successRecordError = document.getElementById('js-success-record-post-error')
+          const successNotice = document.querySelector('.notice')
+
+          successLabelArea.style.display = ''
+          successLabelArea.textContent = data.success_body
+          twoSuccessButton.style.display = ''
+          successTextArea.style.display = 'none'
+          successUpdateCancelButton.style.display = 'none'
+          successRecordError.style.display = 'none'
+          successNotice.innerHTML = '<div>出来たことが編集されました。</div>'
+        })
+        .catch((error) => {
+          console.log(error.message)
+          const successRecordError = document.getElementById('js-success-record-post-error')
+          successRecordError.style.display = ''
+        })
     })
   }
 })
