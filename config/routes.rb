d4bf4341dc14547018a5373bc42eb23c7278b53c
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2021-03-06 15:29:13 +0900

    Add routes, controller and view for terms of use

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   root "home#index"
+
+  get "tos", to: "home#tos"
+
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks",
     registrations: "users/registrations",
